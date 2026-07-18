0ccbb97079baece5ee983c33fe4d9f8d862d366f
Luke Horner <ljhorner2@gmail.com>
2026-07-18 13:16:36 +0100

    countries api call working first draft

--- app.vue
+++ app.vue
@@ -1,15 +1,21 @@
 <template>
-  <LMap
-    :zoom="6"
-    :center="[47.21322, -1.559482]"
-    :use-global-leaflet="false"
-    class="mx-auto !h-[350px] !w-[500px]"
-  >
-    <LTileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-      layer-type="base"
-      name="OpenStreetMap"
-    />
-  </LMap>
+  <div>
+    <pre>{{ countries }}</pre>
+    <LMap
+      :zoom="6"
+      :center="[47.21322, -1.559482]"
+      :use-global-leaflet="false"
+      class="mx-auto !h-[350px] !w-[500px]"
+    >
+      <LTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+    </LMap>
+  </div>
 </template>
+<script setup lang="ts">
+const countries = await useCountriesFetch("");
+</script>
