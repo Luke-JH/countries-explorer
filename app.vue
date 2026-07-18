e3498e5fca70a74323e4c6aaf01292ecc8210390
Luke Horner <ljhorner2@gmail.com>
2026-07-18 11:57:24 +0100

    basic leaflet map with tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <LMap
+    :zoom="6"
+    :center="[47.21322, -1.559482]"
+    :use-global-leaflet="false"
+    class="mx-auto !h-[350px] !w-[500px]"
+  >
+    <LTileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      layer-type="base"
+      name="OpenStreetMap"
+    />
+  </LMap>
 </template>
