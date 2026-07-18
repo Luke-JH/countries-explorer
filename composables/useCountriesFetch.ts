1d1e04f50a13dd835d779cef74a76c652d00b687
Luke Horner <ljhorner2@gmail.com>
2026-07-18 14:54:04 +0100

    remove extra useFetch wrapper, plan to go direct

--- composables/useCountriesFetch.ts
+++
@@ -1,8 +0,0 @@
-import type { UseFetchOptions } from "#app";
-
-export const useCountriesFetch = <T>(
-  path: MaybeRefOrGetter<string>,
-  options: UseFetchOptions<T> = {},
-) => {
-  return useFetchCustom(path, options);
-};
