9580ef490a654b744085b2a8063f144ba0c5459d
Luke Horner <ljhorner2@gmail.com>
2026-07-19 12:51:54 +0100

    app base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
+    app: {
+        baseURL: "/countries-explorer/",
+    },
     devtools: { enabled: true },
     modules: ["@nuxtjs/leaflet", "@nuxtjs/tailwindcss"],
     ssr: false,
