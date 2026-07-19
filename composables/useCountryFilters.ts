8f765eab34c85e9b53d12bbaae65c3fa1d998d47
Luke Horner <ljhorner2@gmail.com>
2026-07-19 12:22:25 +0100

    refine filters composable & new utils

--- composables/useCountryFilters.ts
+++ composables/useCountryFilters.ts
@@ -1,18 +1,7 @@
 import type { Country, IncludeFilter, IncludeFilterKey } from "~/types/country";
+import { matchesIncludeFilter } from "~/utils/includeFilters";
 
 const PAGE_SIZE = 9;
-
-const matchers: Record<IncludeFilterKey, (country: Country) => string[]> = {
-  region: (country) => [country.region],
-  currency: (country) => country.currencies.map((c) => c.name),
-  language: (country) => country.languages.map((l) => l.name),
-};
-
-const matchesIncludeFilter = (country: Country, filter: IncludeFilter) => {
-  const needle = filter.value.trim().toLowerCase();
-  if (!needle) return true;
-  return matchers[filter.key](country).some((value) => value.toLowerCase().includes(needle));
-};
 
 export const useCountryFilters = (countries: Ref<Country[] | null | undefined>) => {
   const search = ref("");
